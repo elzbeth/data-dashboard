a27f6c5ff758f76bc6a0652cc5f1ba2be3598aca
Elzbeth Blanco <elzbethblanco@gmail.com>
2017-12-20 13:51:35 -0600

    second commit
    update

--- README.md
+++ README.md
@@ -6,20 +6,75 @@
 
 ***
 
+## Objetivo
+
+Construi una herramienta web donde los Training Managers de Laboratoria puedan ver estos datos fácil y rápidamente. Y nos comentaron que estos son los datos que revisan normalmente:
+
+* El total de estudiantes presentes por sede y generación.
+* El porcentaje de deserción de estudiantes.
+* La cantidad de estudiantes que superan la meta de puntos en promedio de todos los sprints cursados. La meta de puntos es 70% del total de puntos en HSE y en tech.
+* El porcentaje que representa el dato anterior en relación al total de estudiantes.
+
+* El Net Promoter Score (NPS) promedio de los sprints cursados. El NPS se calcula en base a la encuesta que las estudiantes responden al respecto de la recomendación que darían de Laboratoria, bajo la siguiente fórmula:
+
+[Promoters] = [Respuestas 9 o 10] / [Total respuestas] * 100
+
+[Passive] = [Respuestas 7 u 8] / [Total respuestas] * 100
+
+[Detractors] = [Respuestas entre 1 y 6] / [Total respuestas] * 100
+
+[NPS] = [Promoters] - [Detractors]
+
+* La cantidad y el porcentaje que representa el total de estudiantes que superan la meta de puntos técnicos en promedio y por sprint.
+* La cantidad y el porcentaje que representa el total de estudiantes que superan la meta de puntos de HSE en promedio y por sprint.
+* El porcentaje de estudiantes satisfechas con la experiencia de Laboratoria.
+* La puntuación promedio de l@s profesores.
+* La puntuación promedio de l@s jedi masters.
+
+
+
 ## Flujo de trabajo
 
-1. Debes realizar un [**fork**](https://gist.github.com/ivandevp/1de47ae69a5e139a6622d78c882e1f74)
-   de este repositorio.
+1. Obtener el template del proyecto desde Github.
 
-2. Luego deberás **clonar** tu fork en tu máquina. Recuerda que el comando a usar
-   es `git clone` y su estructura normalmente se ve así:
+2. Establecer una dinámica de trabajo en equipo, incluyendo entre otros puntos:
+  * Herramientas de comunicación entre las miembro del equipo
+  * Herramientas de trabajo colaborativo.
 
-   ```bash
-   git clone https://github.com/<nombre-de-usuario>/freelancer.git
-   ```
+3. Investigar las necesidades de los usuarios (entrevistas y encuestas).
 
-3. Cuando hayas terminado tu producto, envía un Pull Request a este repositorio
-   (puedes solicitar apoyo de tus profes para este paso).
+4. Análisis de competencia (buscar y revisar ejemplos de dashboards ya existentes en el mercado).
 
-> Nota: No olvides que es una buena práctica describir tu proyecto en este
-> archivo `README.md` y también desplegar tu web a Github Pages :smiley:.
+5. Comparar las necesidades de los usuarios con los servicios que ofrecian los dashboards analizados.
+
+6. Elegir de cada opción la que mejor se adpatara al proyecto.
+
+7. Realizar un mockup  y/o wireframe del dashboard.
+
+8. Investigar, seleccionar y estudiar las herramientas que mejor funcionaran para lograr el objetivo.
+
+9. Realización del maquetado para entrevista con el cliente.
+
+10. Planeación de funcionabilidad.
+
+11. Entrevista con el cliente para feedback sobre la maqueta.
+
+12. Ajustes del diseño basado en las observaciones del cliente.
+
+13. Desarrollo de la funcionalidad.
+
+14. Entrvistas de seguimiento con el cliente.
+
+15. Reajustes al proyecto.
+
+16. Continuación de la etapa de desarrollo.
+
+17. Finalización del material para entrega.
+
+18. Code review.
+
+19. Feedback del cliente.
+
+20. Junta de cierre de proyecto.
+
+### Enlace del proyecto:
--- js/app.js
+++ js/app.js
@@ -1,37 +1,12 @@
+/*
+ * Funcionalidad de tu producto
+ */
+// Puedes hacer uso de la base de datos a través de la variable `data`
+console.log(data);
 
-//El total de estudiantes presentes por sede y generación.
-//El porcentaje de deserción de estudiantes.
-//La cantidad de estudiantes que superan la meta de puntos en promedio de todos
-//los sprints cursados. La meta de puntos es 70% del total de puntos en HSE y en tech.
-//El porcentaje que representa el dato anterior en relación al total de
-//estudiantes.
-//El Net Promoter Score (NPS) promedio de los sprints cursados. El NPS se
-//calcula en base a la encuesta que las estudiantes responden al respecto de
-//la recomendación que darían de Laboratoria, bajo la siguiente fórmula:
-//[Promoters] = [Respuestas 9 o 10] / [Total respuestas] * 100
-//[Passive] = [Respuestas 7 u 8] / [Total respuestas] * 100
-//[Detractors] = [Respuestas entre 1 y 6] / [Total respuestas] * 100
-//[NPS] = [Promoters] - [Detractors]
-//La cantidad y el porcentaje que representa el total de estudiantes que superan
-//la meta de puntos técnicos en promedio y por sprint.
-//La cantidad y el porcentaje que representa el total de estudiantes que
-//superan la meta de puntos de HSE en promedio y por sprint.
-//El porcentaje de estudiantes satisfechas con la experiencia de Laboratoria.
-//La puntuación promedio de l@s profesores.
-//La puntuación promedio de l@s jedi masters.
-// var centers = [
-//   {name: 'Arequipa',
-//     key: 'AQP'},
-//   {name: 'Ciudad de México',
-//     key: 'CDMX'},
-//   {name: 'Lima',
-//     key: 'LIM'},
-//   {name: 'Santiago de Chile',
-//     key: 'SCL'}
-// ];
-// alert('hithere');
-var data= data;
- console.log(data)
+// var data = data;
+// alert('hola mundo');
+
 
 //=======VARIABLES=====//
 var campusName;
@@ -41,6 +16,10 @@ var campusId;
 var campusData;
 var generationsByCampus;
 var studentsByCampus;
+var studentName;
+var studentCampus;
+var studentGeneration;
+var studentStatus;
 var studentsEnrolledByCampus;
 var studentsDropOutByCampus
 var averageNotesByStudent;
@@ -62,243 +41,87 @@ var jedisGeneralDirectory;
 var studentsDirectoryByCampus;
 var teachersDirectoryByCampus;
 var jedisDirectoryByCampus;
-//=======EVENTOS=====//
+
+//--- FUNCIONES---//
 
 
-//=======FUNCIONES=====//
 
-//buscar en array//
 function searchInArray(array, string) {
   var result;
-  for (var i = 0; i < array.length; i++) {
-    if (string === array[i]) {
+  for (var m = 0; m < array.length; m++) {
+    if (string === array[m]) {
       result = string;
     }
   }
   return result;
-}
+};
 
-//buscar en objeto//
+// para buscar una key en un objeto
 function searchInObject(obj, string) {
   var result;
   var keys = Object.keys(obj);
-  for (var j = 0; j < keys.length; j++) {
-    if (string === keys[j]) {
+  for (var s = 0; s < keys.length; s++) {
+    if (string === keys[s]) {
       result = string;
     }
   }
   return result;
+};
+
+var campusId = function(){
+   campusId = Object.keys(data);
+  console.log('campusId');
+  console.log(campusId);
+  return campusId;
 }
 
-// var campusId = function(){
-//  var campusId = Object.keys(data);
-//  console.log('campusId');
-//  console.log(campusId);
-// };
+campusId();
 
-// campusId();
 
 var campusData = function(){
- var campusData = Object.values(data);
- console.log('campusData');
- console.log(campusData);
-};
-
-// campusData();
-
-var campusList = function(){
- var campusName = Object.keys(data);
- var campusList =[];
- for (var i = 0; i < campusName.length; i++) {
-   campusList.push([campusName[i]= data[campusName[i]]])
+  campusData= Object.values(data);
+  console.log('campusData');
+  console.log(campusData);
+  return campusData
+}
+campusData();
+ var generationsByCampus = function(){
+   for (var i = 0; i < campusId.length; i++) {
+     for (var j = 0; j < campusId.length; j++) {
+       var generationsByCampus = Object.keys(campusData[j]);
+       console.log('generationsByCampus');
+       console.log(generationsByCampus);
+     }
+   }
  }
- console.log('campusList');
- console.log(campusList);
-};
 
-// campusList();
+ generationsByCampus();
 
-var generation = function(data){
+ var generationsData = function(){
+   for (var i = 0; i < campusId.length; i++) {
+     for (var j = 0; j < campusId.length; j++) {
+       var generationsData = Object.values(campusData[j]);
+       console.log('generationsData');
+       console.log(generationsData);
+     }
+   }
+ }
 
+ generationsData();
 
-};
 
+var studentsKeys = function(){
+   for (var i = 0; i < campusData.length; i++) {
+     for (var j = 0; j < campusData.length; j++) {
+       var studentsKeys = Object.keys(generationsData[j]);
+       console.log('studentsKeys');
+       console.log(studentsKeys);
+     }
+   }
+ }
 
-var campusData = function(data){
+ studentsKeys();
 
 
-};
 
-var generationsByCampus = function(){
 
-};
-
-generationsByCampus();
-
-
-var studentsByCampus = function(data){
-
-
-};
-
-var studentsEnrolledByCampus = function(data){
-
-
-};
-
-var studentsDropOutByCampus = function(data){
-
-
-};
-
-var averageNotesByStudent = function(data){
-
-
-};
-
-var averageNotesByCampus = function(data){
-
-
-};
-
-var averageNotesByGeneration = function(data){
-
-
-};
-
-var averageTechNotesByCampus = function(data){
-
-
-};
-
-var averageTechNotesByGeneration = function(data){
-
-
-};
-
-var averageHsehNotesByCampus = function(data){
-
-
-};
-
-var averageHsehNotesByGeneration = function(data){
-
-
-};
-
-var averageNotesByTeachers = function(data){
-
-
-};
-
-var averageNotesByJedis = function(data){
-
-
-};
-
-var studentsOverAverageByCampus = function(data){
-
-
-};
-
-var studentsUnderAverageByCampu = function(data){
-
-
-};
-
-var averageNpsByCampus = function(data){
-
-
-};
-
-var averageNpsByGeneration = function(data){
-
-
-};
-
-var studentsGeneralDirectory = function(data){
-
-
-};
-
-var teachersGeneralDirectory = function(data){
-
-
-};
-
-var jedisGeneralDirectory = function(data){
-
-
-};
-
-var studentsDirectoryByCampus = function(data){
-
-
-};
-
-var teachersDirectoryByCampus = function(data){
-
-
-};
-
-var jedisDirectoryByCampus = function(data){
-
-
-};
-
-// var showHide = function(e){
-//    var campusSelected = e.target.dataset.campusSelected;
-//    var generalDash = document.getElementById('generalDash');
-//    var campusArequipa = document.getElementById('arequipa');
-//    var campusCdMexico = document.getElementById('mexico');
-//    var campusLima = document.getElementById('lima');
-//    var campusChile = document.getElementById('chile');
-//
-//    if(campusSelected === campusArequipa){
-//      generalDash.style.display = "none";
-//      campusArequipa.style.display = "block";
-//      campusCdMexico.style.display = "none";
-//      campusLima.style.display = "none";
-//      campusChile.style.display = "none";
-//    }else if(campusSelected === campusCdMexico){
-//      generalDash.style.display = "none";
-//      campusArequipa.style.display = "none";
-//      campusCdMexico.style.display = "block";
-//      campusLima.style.display = "none";
-//      campusChile.style.display = "none";
-//    }else if(campusSelected === campusLima){
-//      generalDash.style.display = "none";
-//      campusArequipa.style.display = "none";
-//      campusCdMexico.style.display = "none";
-//      campusLima.style.display = "block";
-//      campusChile.style.display = "none";
-//    }else if(campusSelected === campusChile){
-//      generalDash.style.display = "none";
-//      campusArequipa.style.display = "none";
-//      campusCdMexico.style.display = "none";
-//      campusLima.style.display = "none";
-//      campusChile.style.display = "block";
-//    }
-//    console.log('control 1');
-// }
-//}
-
-// var loadingPage = function(){
-//   var campusArequipa = document.getElementById('arequipa');
-//   var campusCdMexico = document.getElementById('mexico');
-//   var campusLima = document.getElementById('lima');
-//   var campusChile = document.getElementById('chile');
-//   generalDash.style.display ="block";
-//   campusArequipa.style.display = "none";
-//   campusCdMexico.style.display = "none";
-//   campusLima.style.display = "none";
-//   campusChile.style.display = "none";
-//
-//   var elementsCampus = document.getElementsByClassName('campusMenu');
-//   for (var i = 0; i < elementsCampus.length; i++) {
-//     elementsCampus[i].addEventListener('click', showHide );
-//   }
-//   console.log('control 2');
-// }
-//
-// loadingPage();
